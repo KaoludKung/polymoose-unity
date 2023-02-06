c13604b6e26ec7533d26a277aad3eeda5135673d
Chanakan Kueaworakulchai <94474006+Chanakan262@users.noreply.github.com>
2023-02-07 01:27:06 +0700

    play

--- Polymoose/Assets/Scripts/QuizManager.cs
+++ Polymoose/Assets/Scripts/QuizManager.cs
@@ -78,6 +78,8 @@ public class QuizManager : MonoBehaviour
         hintCountText.text = hintCount.ToString();
         freezeCountText.text = freezeCount.ToString();
         extraCountText.text = extraCount.ToString();
+
+        MusicPlay.instance.GetComponent<AudioSource>().Pause();
     }
 
     // Start is called before the first frame update
