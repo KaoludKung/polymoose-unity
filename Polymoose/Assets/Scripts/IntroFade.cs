6829659a8962002f2ffb9d980c8c58446101ac05
Chanakan Kueaworakulchai <94474006+Chanakan262@users.noreply.github.com>
2023-03-03 07:04:56 +0700

    update 2.3

--- Polymoose/Assets/Scripts/IntroFade.cs
+++ Polymoose/Assets/Scripts/IntroFade.cs
@@ -7,10 +7,13 @@ using TMPro;
 public class IntroFade : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI chapterText;
-    [SerializeField] private string scene;
+    private string scene;
+    private int loading;
 
     private void Awake()
     {
+        loading = PlayerPrefs.GetInt("Loading", loading);
+        SetText(loading);
         MusicPlay.instance.GetComponent<AudioSource>().Pause();
         chapterText.canvasRenderer.SetAlpha(0.0f);
     }
@@ -28,5 +31,32 @@ public class IntroFade : MonoBehaviour
         chapterText.CrossFadeAlpha(0.0f, 2.0f, false);
         yield return new WaitForSeconds(2.0f);
         SceneManager.LoadScene(scene);
+    }
+
+    void SetText(int id)
+    {
+        switch (id)
+        {
+            case 11:
+                chapterText.text = "Chapter 1 <space=8em> Interview";
+                scene = "Walk1";
+                break;
+            case 12:
+                chapterText.text = "Chapter 2 <space=10em> Talking after work";
+                scene = "VisualQuiz2";
+                break;
+            case 13:
+                chapterText.text = "Chapter 3 <space=8em> Business meals";
+                scene = "Walk3";
+                break;
+            case 14:
+                chapterText.text = "Chapter 4 <space=8em> Business calling";
+                scene = "Walk4";
+                break;
+            case 15:
+                chapterText.text = "Chapter 5 <space=10em> Dealing with an angry customer";
+                scene = "Walk5";
+                break;
+        }
     }
 }
