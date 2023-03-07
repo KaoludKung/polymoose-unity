3ba1cfebe29229a3d05b6f3d5d7d10d571b343ad
Chanakan Kueaworakulchai <94474006+Chanakan262@users.noreply.github.com>
2023-03-08 03:26:23 +0700

    black screen edit

---
+++ Polymoose/Assets/Scripts/VideoAlert.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoAlert : MonoBehaviour
+{
+    public VideoAlpha videoAlpha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoAlpha.isPlay = true;
+    }
+    
+}
--- Polymoose/Assets/Scripts/VideoAlpha.cs
+++ Polymoose/Assets/Scripts/VideoAlpha.cs
@@ -2,21 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.Video;
 
 public class VideoAlpha : MonoBehaviour
 {
     [SerializeField] private RawImage videoImage;
+    [SerializeField] private VideoPlayer videoPlayer;
     [SerializeField] private float time;
+    
+    public bool isPlay;
+    private bool isStop = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("SetAlpha", time);
+        videoPlayer.Prepare();
+        videoImage.color = new Color(1.0f, 1.0f, 1.0f, 0f);
     }
 
-    void SetAlpha()
+    void Update()
     {
-        videoImage.color = new Color(1.0f, 1.0f, 1.0f, 1f);
+        if (isPlay)
+        {
+            PlayVideo();
+            isStop = true;
+        }
+    }
+
+    void PlayVideo()
+    {
+        if (!isStop)
+        {
+            videoImage.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            videoPlayer.Play();
+        }
     }
 }
