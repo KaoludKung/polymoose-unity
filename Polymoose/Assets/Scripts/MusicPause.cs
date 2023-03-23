e1003676933b56bfcbf66de25e9b8c0c15f4fdc1
Chanakan Kueaworakulchai <94474006+Chanakan262@users.noreply.github.com>
2023-03-24 02:24:30 +0700

    asshole

--- Polymoose/Assets/Scripts/MusicPause.cs
+++ Polymoose/Assets/Scripts/MusicPause.cs
@@ -7,7 +7,11 @@ public class MusicPause : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MusicPlay.instance.GetComponent<AudioSource>().Pause();
+        MusicPlay.instance.GetComponent<AudioSource>().Stop();
     }
 
+    void Update()
+    {
+        MusicPlay.instance.GetComponent<AudioSource>().Stop();
+    }
 }
